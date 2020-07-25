e7cbdafbcf06df6590e8e63a105d80979e33d87b
aburd <burdick.aaron@gmail.com>
2020-07-26 03:43:36 +0900

    add desktop

---
+++ src/Desktop.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Desktop.scss'
+
+export default function Desktop() {
+  return (
+    <div className="Desktop">
+      <h1>Test</h1>
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Desktop from './Desktop';
 import * as serviceWorker from './serviceWorker';
 import './styles/main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Desktop />
   </React.StrictMode>,
   document.getElementById('root')
 );
