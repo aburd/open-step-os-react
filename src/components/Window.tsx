73e72de566a9827e1a03ede21bb1338279bde597
aburd <burdick.aaron@gmail.com>
2020-07-27 06:18:59 +0900

    fix drag

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import Draggable from 'react-draggable'
 import WindowHeader from './WindowHeader'
 import './Window.scss'
@@ -19,19 +19,31 @@ const Window: React.SFC<Props> = ({
   title,
   active,
   initialPosition,
+  initialWidth,
   children,
 }) => {
+  const headerRef = useRef(null)
+  const [width, updateWidth] = useState(initialWidth || 100)
   return (
     <Draggable
       defaultPosition={initialPosition}
+      onStart={(e) => {
+        // @ts-ignore
+        const classes = [...e.target.classList]
+        if (!['Bar', 'WindowHeader--inner'].some(c => classes.includes(c))) {
+          return false
+        }
+      }}
     >
-      <div className="Window">
-        <WindowHeader
-          text={title}
-          active={active}
-          textCenter={active}
-          onCloseClick={() => alert('Clicked')}
-        />
+      <div className="Window" style={{ width }}>
+        <span ref={headerRef}>
+          <WindowHeader
+            text={title}
+            active={active}
+            textCenter={active}
+            onCloseClick={() => alert('Clicked')}
+          />
+        </span>
         <div className="Window--inner">
           {children}
         </div>
